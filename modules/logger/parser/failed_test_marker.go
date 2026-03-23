fb25cc7fad00e698bf3c629310a95a3acf1985b0
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2026-03-23 16:22:41 -0400

    chore: Expanding linting to `logger`

--- modules/logger/parser/failed_test_marker.go
+++ modules/logger/parser/failed_test_marker.go
@@ -1,4 +1,4 @@
-// Package logger/parser contains methods to parse and restructure log output from go testing and terratest
+// Package parser contains methods to parse and restructure log output from go testing and terratest.
 package parser
 
 // TestResultMarker tracks the indentation level of a test result line in go test output.
@@ -17,50 +17,58 @@ type TestResultMarker struct {
 	IndentLevel int
 }
 
-// TestResultMarkerStack is a stack data structure to store TestResultMarkers
+// TestResultMarkerStack is a stack data structure to store TestResultMarkers.
 type TestResultMarkerStack []TestResultMarker
 
-// A blank TestResultMarker is considered null. Used when peeking or popping an empty stack.
-var NULL_TEST_RESULT_MARKER = TestResultMarker{}
+// NullTestResultMarker is a blank TestResultMarker considered null. Used when peeking or popping an empty stack.
+var NullTestResultMarker = TestResultMarker{}
 
-// TestResultMarker.push will push a TestResultMarker object onto the stack, returning the new one.
-func (s TestResultMarkerStack) push(v TestResultMarker) TestResultMarkerStack {
+// NULL_TEST_RESULT_MARKER is deprecated: Use NullTestResultMarker instead.
+//
+//nolint:staticcheck // preserving old name for backwards compatibility
+var NULL_TEST_RESULT_MARKER = NullTestResultMarker //nolint:revive
+
+// Push will push a TestResultMarker object onto the stack, returning the new one.
+func (s TestResultMarkerStack) Push(v TestResultMarker) TestResultMarkerStack {
 	return append(s, v)
 }
 
-// TestResultMarker.pop will pop a TestResultMarker object off of the stack, returning the new one with the popped
+// Pop will pop a TestResultMarker object off of the stack, returning the new one with the popped
 // marker.
 // When stack is empty, will return an empty object.
-func (s TestResultMarkerStack) pop() (TestResultMarkerStack, TestResultMarker) {
+func (s TestResultMarkerStack) Pop() (TestResultMarkerStack, TestResultMarker) {
 	l := len(s)
 	if l == 0 {
-		return s, NULL_TEST_RESULT_MARKER
+		return s, NullTestResultMarker
 	}
+
 	return s[:l-1], s[l-1]
 }
 
-// TestResultMarker.peek will return the top TestResultMarker from the stack, but will not remove it.
-func (s TestResultMarkerStack) peek() TestResultMarker {
+// Peek will return the top TestResultMarker from the stack, but will not remove it.
+func (s TestResultMarkerStack) Peek() TestResultMarker {
 	l := len(s)
 	if l == 0 {
-		return NULL_TEST_RESULT_MARKER
+		return NullTestResultMarker
 	}
+
 	return s[l-1]
 }
 
-// TestResultMarker.isEmpty will return whether or not the stack is empty.
-func (s TestResultMarkerStack) isEmpty() bool {
+// IsEmpty will return whether or not the stack is empty.
+func (s TestResultMarkerStack) IsEmpty() bool {
 	return len(s) == 0
 }
 
-// removeDedentedTestResultMarkers will pop items off of the stack of TestResultMarker objects until the top most item
+// RemoveDedentedTestResultMarkers will pop items off of the stack of TestResultMarker objects until the top most item
 // has an indent level less than the current indent level.
 // Assumes that the stack is ordered, in that recently pushed items in the stack have higher indent levels.
-func (s TestResultMarkerStack) removeDedentedTestResultMarkers(currentIndentLevel int) TestResultMarkerStack {
+func (s TestResultMarkerStack) RemoveDedentedTestResultMarkers(currentIndentLevel int) TestResultMarkerStack {
 	// This loop is a garbage collection of the stack, where it removes entries every time we dedent out of a fail
 	// block.
-	for !s.isEmpty() && s.peek().IndentLevel >= currentIndentLevel {
-		s, _ = s.pop()
+	for !s.IsEmpty() && s.Peek().IndentLevel >= currentIndentLevel {
+		s, _ = s.Pop()
 	}
+
 	return s
 }
--- modules/logger/parser/store.go
+++ modules/logger/parser/store.go
@@ -1,4 +1,3 @@
-// Package logger/parser contains methods to parse and restructure log output from go testing and terratest
 package parser
 
 import (
@@ -12,32 +11,38 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// LogWriter manages file handles for writing test log output, keyed by test name.
 type LogWriter struct {
-	// Represents an open file to a log corresponding to a test (key = test name)
-	lookup    map[string]*os.File
-	outputDir string
+	// Lookup represents an open file to a log corresponding to a test (key = test name).
+	Lookup map[string]*os.File
+	// OutputDir is the directory where log files are written.
+	OutputDir string
 }
 
-// LogWriter.getOrCreateFile will get the corresponding file to a log for the provided test name, or create a new file.
-func (logWriter LogWriter) getOrCreateFile(logger *logrus.Logger, testName string) (*os.File, error) {
-	file, hasKey := logWriter.lookup[testName]
+// GetOrCreateFile will get the corresponding file to a log for the provided test name, or create a new file.
+func (logWriter LogWriter) GetOrCreateFile(logger *logrus.Logger, testName string) (*os.File, error) {
+	file, hasKey := logWriter.Lookup[testName]
 	if hasKey {
 		return file, nil
 	}
 
-	filename := filepath.Join(logWriter.outputDir, testName+".log")
+	filename := filepath.Join(logWriter.OutputDir, testName+".log")
+
 	file, err := createLogFile(logger, filename)
 	if err != nil {
 		return nil, errors.WithStackTrace(err)
 	}
-	logWriter.lookup[testName] = file
+
+	logWriter.Lookup[testName] = file
+
 	return file, nil
 }
 
-// LogWriter.closeChannels closes all the channels in the lookup dictionary
-func (logWriter LogWriter) closeFiles(logger *logrus.Logger) {
+// CloseFiles closes all the file handles in the Lookup dictionary.
+func (logWriter LogWriter) CloseFiles(logger *logrus.Logger) {
 	logger.Infof("Closing all the files in log writer")
-	for testName, file := range logWriter.lookup {
+
+	for testName, file := range logWriter.Lookup {
 		err := file.Close()
 		if err != nil {
 			logger.Errorf("Error closing log file for test %s: %s", testName, err)
@@ -45,19 +50,24 @@ func (logWriter LogWriter) closeFiles(logger *logrus.Logger) {
 	}
 }
 
-// writeLog will write the provided text to the corresponding log file for the provided test.
-func (logWriter LogWriter) writeLog(logger *logrus.Logger, testName string, text string) error {
-	file, err := logWriter.getOrCreateFile(logger, testName)
+// WriteLog will write the provided text to the corresponding log file for the provided test.
+func (logWriter LogWriter) WriteLog(logger *logrus.Logger, testName string, text string) error {
+	file, err := logWriter.GetOrCreateFile(logger, testName)
 	if err != nil {
 		logger.Errorf("Error retrieving log for test: %s", testName)
 		return errors.WithStackTrace(err)
 	}
+
 	_, err = file.WriteString(text + "\n")
 	if err != nil {
 		logger.Errorf("Error (%s) writing log entry: %s", err, text)
 		return errors.WithStackTrace(err)
 	}
-	file.Sync()
+
+	if err := file.Sync(); err != nil {
+		return errors.WithStackTrace(err)
+	}
+
 	return nil
 }
 
@@ -66,41 +76,53 @@ func (logWriter LogWriter) writeLog(logger *logrus.Logger, testName string, text
 func createLogFile(logger *logrus.Logger, filename string) (*os.File, error) {
 	// We extract and create the directory for interpolated filename, to handle nested tests where testname contains '/'
 	dirName := filepath.Dir(filename)
-	err := ensureDirectoryExists(logger, dirName)
+
+	err := EnsureDirectoryExists(logger, dirName)
 	if err != nil {
 		return nil, errors.WithStackTrace(err)
 	}
+
 	file, err := os.Create(filename)
 	if err != nil {
 		return nil, errors.WithStackTrace(err)
 	}
+
 	return file, nil
 }
 
-// ensureDirectoryExists will only attempt to create the directory if it does not exist
-func ensureDirectoryExists(logger *logrus.Logger, dirName string) error {
+// EnsureDirectoryExists will only attempt to create the directory if it does not exist.
+func EnsureDirectoryExists(logger *logrus.Logger, dirName string) error {
 	if files.IsDir(dirName) {
 		logger.Infof("Directory %s already exists", dirName)
 		return nil
 	}
+
 	logger.Infof("Creating directory %s", dirName)
+
 	err := os.MkdirAll(dirName, os.ModePerm)
 	if err != nil {
 		logger.Errorf("Error making directory %s: %s", dirName, err)
 		return errors.WithStackTrace(err)
 	}
+
 	return nil
 }
 
-// storeJunitReport takes a parsed Junit report and stores it as report.xml in the output directory
+// storeJunitReport takes a parsed Junit report and stores it as report.xml in the output directory.
 func storeJunitReport(logger *logrus.Logger, outputDir string, report *junitparser.Report) {
-	ensureDirectoryExists(logger, outputDir)
+	if err := EnsureDirectoryExists(logger, outputDir); err != nil {
+		logger.Errorf("Error ensuring output directory exists: %s", err)
+		return
+	}
+
 	filename := filepath.Join(outputDir, "report.xml")
+
 	f, err := os.Create(filename)
 	if err != nil {
 		logger.Errorf("Error making file %s for junit report", filename)
 		return
 	}
+
 	defer f.Close()
 
 	err = junitformatter.JUnitReportXML(report, false, "", f)
